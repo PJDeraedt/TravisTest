7cc051d3ebb54ade7e3dee90e425b4541c882538
DERAEDPI <Pieter-Jan.DERAEDT@external.danone.com>
2018-12-17 13:59:26 +0000

    karma config

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -2,12 +2,16 @@
 module.exports = function(config) {
 	require("./karma.conf")(config);
 	config.set({
-
+		
+		basePath: 'webapp',
+		
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
 			'{,!(test)}/*.js': ['coverage']
 		},
+
+		frameworks: ['qunit'],
 
 		coverageReporter: {
 			includeAllSources: true,
