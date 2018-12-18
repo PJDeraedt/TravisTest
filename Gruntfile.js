d66ad6065dad0315d64e3c4b6828c513701c1256
DERAEDPI <Pieter-Jan.DERAEDT@external.danone.com>
2018-12-18 08:29:25 +0000

    mta

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function (grunt) {
 	"use strict";
+	//grunt.loadNpmTasks("grunt-sapui5");
+	//grunt.registerTask("unit_and_integration_tests", ["test"]);
 	/*
-	grunt.loadNpmTasks("grunt-sapui5");
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
-	grunt.registerTask("unit_and_integration_tests", ["test"]);
 	grunt.config.merge({
 		coverage_threshold: {
 			statements: 0,
@@ -13,4 +13,10 @@ module.exports = function (grunt) {
 		}
 	});
 	*/
+	grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
+    grunt.registerTask('default', [
+        'lint',
+        'clean',
+        'build'
+    ]);
 };
